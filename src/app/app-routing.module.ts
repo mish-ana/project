fb1a6c0e7e6f124c7bc92ceeca642d01170a80ed
abhi-2605 <abhishekmenon2605@gmail.com>
2021-04-21 00:41:15 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashComponent } from './dash/dash.component';
 import { Dash2Component } from './dash2/dash2.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NavComponent } from './nav/nav.component';
 import { SignupComponent } from './signup/signup.component';
@@ -9,7 +10,7 @@ import { UpdateComponent } from './update/update.component';
 import { UserComponent } from './user/user.component';
 import { UservComponent } from './userv/userv.component';
 
-const routes: Routes = [{path:"login", component:LoginComponent},{path:"login/signup",component:SignupComponent},{path:"nav",component:NavComponent,children:[{
+const routes: Routes = [{path:"",component:HomeComponent},{path:"login", component:LoginComponent},{path:"login/signup",component:SignupComponent},{path:"nav",component:NavComponent,children:[{
   path:"dash",component:DashComponent
 },{path:"user",component:UserComponent} ,{path:"userv",component:UservComponent },{path:"dash2",component:Dash2Component},{path:"update",component:UpdateComponent}]}
 ];
